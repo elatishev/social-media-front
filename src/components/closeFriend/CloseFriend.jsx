39aa983de6b41cf591310bb4d2a8c09975202412
Evgenii Latyshev <evgeniy.latishev@yandex.ru>
2022-01-06 01:44:27 +0400

    Added base data fetching

--- src/components/closeFriend/CloseFriend.jsx
+++ src/components/closeFriend/CloseFriend.jsx
@@ -1,9 +1,10 @@
+import { makeRelativePath } from "../../mainConstants";
 import "./closeFriend.css";
 
 export default function CloseFriend({ user }) {
   return (
     <li className="sidebarFriend">
-      <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
+      <img className="sidebarFriendImg" src={makeRelativePath(user.profilePicture)} alt="" />
       <span className="sidebarFriendName">{user.username}</span>
     </li>
   );
--- src/components/online/Online.jsx
+++ src/components/online/Online.jsx
@@ -1,10 +1,11 @@
+import { makeRelativePath } from "../../mainConstants";
 import "./online.css";
 
 export default function Online({ user }) {
   return (
     <li className="rightbarFriend">
       <div className="rightbarProfileImgContainer">
-        <img className="rightbarProfileImg" src={user.profilePicture} alt="" />
+        <img className="rightbarProfileImg" src={makeRelativePath(user.profilePicture)} alt="" />
         <span className="rightbarOnline"></span>
       </div>
       <span className="rightbarUsername">{user.username}</span>
